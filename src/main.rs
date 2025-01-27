d24c9ea13ad813565012842b3dd5dc1b18941038
Cori <kgaluba@us.edu.pl>
2025-01-27 01:21:20 +0100

    testing inline asm in preparation for interrupt table creation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::arch::asm;
+
 use log::info;
 use uefi::prelude::*;
 
@@ -8,6 +10,18 @@ use uefi::prelude::*;
 fn main() -> Status {
     uefi::helpers::init().unwrap();
     info!("Hello world!");
+    let mut x: u64 = 4;
+unsafe {
+    asm!(
+        "mov {tmp}, {x}",
+        "shl {tmp}, 1",
+        "shl {x}, 2",
+        "add {x}, {tmp}",
+        x = inout(reg) x,
+        tmp = out(reg) _,
+    );
+}
+    info!("generalnie to chyba dziala to cale asm : {} | normalne mnozenie: {}", x, 4 * 6);
     boot::stall(10_000_000);
     Status::SUCCESS
 }
